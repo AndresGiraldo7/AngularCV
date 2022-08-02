d7e706cb86e7e4326dfbb58004ceac2563ff1fb9
AndresGiraldo7 <and.th420@gmail.com>
2022-08-02 17:06:42 -0500

    commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/curriculumVitaeCopia'));
+app.use(express.static('./dist/curriculum-vitae'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/curriculumVitaeCopia/src/index.html'}),
+    res.sendFile('index.html', {root: 'dist/curriculum-vitae/index.html'}),
 );
 
 // Start the app by listening on the default Heroku port
