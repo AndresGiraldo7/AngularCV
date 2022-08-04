00d371ad4275f61cd9de7948ea156b9da4e9c893
AndresGiraldo7 <and.th420@gmail.com>
2022-08-04 12:06:48 -0500

    commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/curriculum-vitae/index.html' })
 );
 
-app.get('/family-reference', (req, res) =>
+app.get('/*', (req, res) =>
     res.sendFile(path.join(__dirname, './src', 'family-reference.html'))
 );
 
